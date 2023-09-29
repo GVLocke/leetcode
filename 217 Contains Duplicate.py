3d63703655fea495196a1ef9b2bb18ebfc881677
G Locke <jkeys726@gmail.com>
2023-09-29 17:13:59 -0500

    Finished problem 217

---
+++ 217 Contains Duplicate.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def __self__(self):
+        pass
+
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums_set = set()
+        for i in nums:
+            if i in nums_set:
+                return True
+            else:
+                nums_set.add(i)
+        return False
+
+nums2 = [1,2,3,4]
+bruh = Solution()
+print(bruh.containsDuplicate(nums2))
