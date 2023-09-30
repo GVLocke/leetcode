29da4eefb84e6f07395c90576fb7b5a83f9db462
G Locke <jkeys726@gmail.com>
2023-09-29 19:12:44 -0500

    solved 242, changed directory organization

---
+++ solutions/242-Valid-Anagram /242 Valid Anagram.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def __self__(self):
+        pass
+
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_list = list(s)
+        s_list.sort()
+        t_list = list(t)
+        t_list.sort()
+        return s_list == t_list
+    
+text = "bruh"
+text2 = "rubt"
+bruh = Solution()
+print(bruh.isAnagram(text, text2))
